a1361c0bc0ac56150d4f446bfc9c5054d64a1439
walner <git@walner.cl>
2025-01-10 18:15:00 -0600

    add co

--- routes/libros.js
+++ routes/libros.js
@@ -12,6 +12,7 @@ const db = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    connectTimeout: 10000,
 });
 
 db.connect((err) => {
